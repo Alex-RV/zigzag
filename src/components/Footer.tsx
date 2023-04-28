d9dff9423a1749f1ae3370c4135a1adfc3624f09
Alex-RV <59423285+Alex-RV@users.noreply.github.com>
2023-04-28 11:38:46 -0700

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,35 +6,34 @@ import Image from 'next/image';
 export default function Footer() {
   return (
     <footer className="flex flex-col justify-center items-center max-w-7xl mx-auto w-full mb-8">
-  <hr className="w-full border-1 border-gray-200 dark:border-gray-800 mb-6" />
-  <div className="w-full max-w-7xl grid grid-cols-1 gap-4 sm:grid-cols-3 sm:text-sm sm:space-y-2">
-    <div className="flex flex-col space-y-4 justify-center items-center">
-      <Link
-        href="/#about"
-        className="text-[#BEBCAD] text-xl sm:text-base font-extrabold hover:text-gray-600 transition"
-      >
-        About
-      </Link>
-    </div>
-    <div className="flex flex-col space-y-4 justify-center items-center">
-        <Image width={120} height={120} src={'/logo.svg'} alt={'bg'}/> 
-        <Link
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://www.linkedin.com/in/alex-riabov-v/" className="text-[#BEBCAD] text-sm sm:text-base font-extrabold hover:text-gray-600 transition">
-            @2023 Alex-RV
-        </Link>
-    </div>
-    <div className="flex flex-col space-y-4 justify-center items-center">
-      <Link
-        href="/projects"
-        className="text-[#BEBCAD] text-xl sm:text-base font-extrabold hover:text-gray-600 transition"
-      >
-        Register
-      </Link>
-    </div>
-  </div>
-</footer>
-
+      <hr className="w-full border-1 border-gray-200 dark:border-gray-800 mb-6" />
+      <div className="w-full max-w-7xl grid grid-cols-1 gap-4 sm:grid-cols-3">
+        <div className="flex flex-col space-y-4 justify-center items-center">
+          <Link
+            href="/#about"
+            className="text-[#BEBCAD] text-2xl font-extrabold hover:text-gray-600 transition"
+          >
+            About
+          </Link>
+        </div>
+        <div className="flex flex-col space-y-4 justify-center items-center">
+            <Image width={120} height={120} src={'/logo.svg'} alt={'bg'}/> 
+            <Link
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.linkedin.com/in/alex-riabov-v/" className="text-[#BEBCAD] text-sm font-extrabold hover:text-gray-600 transition">
+                @2023 Alex-RV
+            </Link>
+        </div>
+        <div className="flex flex-col space-y-4 justify-center items-center">
+          <Link
+            href="/projects"
+            className="text-[#BEBCAD] text-2xl font-extrabold hover:text-gray-600 transition"
+          >
+            Register
+          </Link>
+        </div>
+      </div>
+    </footer>
   );
 }
